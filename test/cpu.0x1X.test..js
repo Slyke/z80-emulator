e051f473f8c43343ad43df3bfeb5b7a6f0dccd84
Slyke (Desktop) <steven.lawler777@gmail.com>
2018-09-05 00:48:54 -0400

    Started on unit tests for 0x1X opcodes

--- test/cpu.0x1X.test..js
+++ test/cpu.0x1X.test..js
@@ -70,5 +70,74 @@ describe('Z80 CPU Emulator', () => {
       });
     });
 
+    describe('0x11 - LD2R', () => {
+      cpu = cpuFunc();
+      var cpuInputState = Object.freeze({
+        ...cpu,
+        flags: {
+          pc: 0,
+          d: 0x01,
+          e: 0x02
+        },
+        memory: [ 0x11, 0x04, 0x05 ]
+      });
+
+      var modifiyState = JSON.parse(JSON.stringify(cpuInputState));
+
+      var disassembleOut = cpu.disassemble8080OP(cpuInputState, cpuInputState.flags.pc);
+      var cpuOut = cpu.emulate8080OP(modifiyState, cpuInputState.flags.pc);
+      var cycleDifference = (modifiyState.cycles - (cpuInputState.cycles ? cpuInputState.cycles : 0));
+
+      var shouldCycles = 10;         // *** Cycles
+      var shouldPC = 3;             // *** PC Count
+      var shouldOPCode = 'LD2R';     // *** OP Code
+
+      //Basic tests
+      it('should have matching cycle count between the disassembler and emulator', () => {
+        assert.equal(cycleDifference, disassembleOut.cycles);
+      });
+      it(`should have matching cycle count of:  ${shouldCycles}`, () => {
+        assert.equal(cycleDifference, shouldCycles);
+      });
+      it('should return 0 (valid OP code)', () => {
+        assert.equal(cpuOut, 0);
+      });
+      it('PC should increase by disassembler amount', () => {
+        assert.equal((modifiyState.flags.pc - cpuInputState.flags.pc), disassembleOut.opBytes);
+      });
+      it(`PC should increase by:  ${shouldPC}`, () => {
+        assert.equal((modifiyState.flags.pc - cpuInputState.flags.pc), shouldPC);
+      });
+      it(`Disassembler should return correct OP code: '${shouldOPCode}'`, () => {
+        assert.equal(disassembleOut.opCode, shouldOPCode);
+      });
+
+      // OP Code specific tests
+      it(`Register 'D' should equal 0x04`, () => {
+        assert.equal(modifiyState.flags.d, 0x05);
+      });
+      it(`Register 'E' should equal 0x05`, () => {
+        assert.equal(modifiyState.flags.e, 0x04);
+      });
+
+      // Disassembler specific checks
+      it(`Disassembler has set oreg`, () => {
+        assert.equal(disassembleOut.oreg, "DE");
+      });
+      it(`Disassembler has not set ireg`, () => {
+        assert.equal(disassembleOut.ireg, "");
+      });
+      it(`Disassembler should not have ptr set`, () => {
+        assert.equal(disassembleOut.ptr, "");
+      });
+      it(`Disassembler should not have conditional cycle checked`, () => {
+        assert.equal(disassembleOut.cycleConditional, false);
+      });
+      it(`Disassembler should have p1 and p2 set`, () => {
+        assert.equal(disassembleOut.para1, 0x04);
+        assert.equal(disassembleOut.para2, 0x05);
+      });
+    });
+
   });
 });
